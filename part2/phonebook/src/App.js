6226582b3586dc901299c9dc4f32ccff0cad6c69
Long Mai <longm89@gmail.com>
2021-02-02 15:20:03 +0100

    finish up to 2.9, still need to re-factoring the codes into components

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,43 +8,61 @@ const Person = ({name, number}) => {
   )
 }
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: "040-1234567"}
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filterWord, setFilterWord] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     //prevent the submit from default handling of the form
     event.preventDefault()
+
     const nameObject = {
       name: newName,
       number: newNumber
     }
+
     const isEqual = (element) => element.name === newName 
+
     if (persons.findIndex(isEqual) !== -1) {
       alert(`${newName} is already added to phonebook`)
       return 
     }
+
     setPersons(persons.concat(nameObject))
     setNewName("")
     setNewNumber("")
   }
   
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilterWord(event.target.value)
+  }
+  function containFilter (person, filter) {
+    let lowerCaseName = person.name.toLowerCase()
+    let lowerCaseFilter = filter.toLowerCase()
+    return lowerCaseName.indexOf(lowerCaseFilter) !== -1
+  }
+  const personsToShow = persons.filter(person => containFilter(person, filterWord))
+    //define the personsToShow to be a list of people to show, depending on the filter
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit = {addName}>
+      <div>filter shown with <input value = {filterWord} onChange = {handleFilter}/></div>
+      <h3>add a new</h3>
+      <form onSubmit = {addPerson}>
         <div>
           name: <input value = {newName} onChange = {handleNameChange}/>
         </div>
@@ -53,8 +71,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-        {persons.map(person => <Person name = {person.name} number = {person.number} key = {person.name}/>)}
+      <h3>Numbers</h3>
+        {personsToShow.map(person => <Person name = {person.name} number = {person.number} key = {person.name}/>)}
     </div>
   )
 }
