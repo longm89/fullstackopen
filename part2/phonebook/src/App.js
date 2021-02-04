edfe8804aae612ce7edc066a6f1b31c51a00717a
Long Mai <longm89@gmail.com>
2021-02-04 11:24:31 +0100

    add data fetching to phonebook app

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import axios from "axios"
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterWord, setFilterWord] = useState('')
 
+  const hook = () => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then (response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook, [])
+  
   const addPerson = (event) => {
     //prevent the submit from default handling of the form
     event.preventDefault()
