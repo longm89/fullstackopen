57d34a5acfbf4617201a437efbd1301ad5b925c1
Long Mai <longm89@gmail.com>
2021-02-05 11:57:08 +0100

    add the json server to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,43 +3,67 @@ import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import axios from "axios"
+import personService from "./services/persons"
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filterWord, setFilterWord] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterWord, setFilterWord] = useState('')
 
   const hook = () => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then (response => {
-        console.log("promise fulfilled")
-        setPersons(response.data)
+    personService.getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }
   useEffect(hook, [])
-  
+
   const addPerson = (event) => {
     //prevent the submit from default handling of the form
     event.preventDefault()
 
-    const nameObject = {
+    const personObject = {
       name: newName,
       number: newNumber
     }
 
-    const isEqual = (element) => element.name === newName 
-
-    if (persons.findIndex(isEqual) !== -1) {
-      alert(`${newName} is already added to phonebook`)
-      return 
+    const isEqual = (element) => element.name === newName
+    const indexFound = persons.findIndex(isEqual)
+    if (indexFound !== -1) {
+      const result = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
+      if (result) {
+        let id = persons[indexFound].id
+        personService
+          .update(id, personObject)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+          })
+      }
+    } else {
+      personService.create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName("")
+          setNewNumber("")
+        })
     }
 
-    setPersons(persons.concat(nameObject))
-    setNewName("")
-    setNewNumber("")
+
+
   }
-  
+
+  const deletePerson = (props) => {
+    const result = window.confirm(`Delete ${props.name} ?`)
+    if (!result) {
+      return
+    }
+
+    personService.deletePerson(props.id)
+      .then(response => {
+        setPersons(persons.filter(person => person.id !== props.id))
+      })
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -51,27 +75,27 @@ const App = () => {
   const handleFilter = (event) => {
     setFilterWord(event.target.value)
   }
-  function containFilter (person, filter) {
+  function containFilter(person, filter) {
     let lowerCaseName = person.name.toLowerCase()
     let lowerCaseFilter = filter.toLowerCase()
     return lowerCaseName.indexOf(lowerCaseFilter) !== -1
   }
   const personsToShow = persons.filter(person => containFilter(person, filterWord))
-    //define the personsToShow to be a list of people to show, depending on the filter
+  //define the personsToShow to be a list of people to show, depending on the filter
   return (
     <div>
       <h2>Phonebook</h2>
-        <Filter filterWord = {filterWord} handleFilter = {handleFilter}/>
+      <Filter filterWord={filterWord} handleFilter={handleFilter} />
       <h3>add a new</h3>
-        <PersonForm 
-          addPerson ={addPerson} 
-          newName = {newName} 
-          handleNameChange = {handleNameChange}
-          newNumber = {newNumber} 
-          handleNumberChange = {handleNumberChange}
-        />
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
       <h3>Numbers</h3>
-        <Persons personsToShow = {personsToShow}/>
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,20 +1,26 @@
 import React from "react"
-const Person = ({name, number}) => {
-    return (
-      <div>
-        {name} {number}
-      </div>
-    )
-  }
-  
-  
+const Person = ({ name, number, deletePerson }) => {
+  return (
+    <div>
+      {name} {number}
+      <button onClick={deletePerson}> delete</button>
+    </div>
+  )
+}
+
+
 const Persons = (props) => {
-    return (
-      <div>
-      {props.personsToShow.map(person => 
-        <Person name = {person.name} number = {person.number} key = {person.name}/>)
+  return (
+    <div>
+      {props.personsToShow.map(person =>
+        <Person
+          name={person.name}
+          number={person.number}
+          key={person.name}
+          deletePerson={() => props.deletePerson(person)}
+        />)
       }
-      </div>
-    )
-  }
+    </div>
+  )
+}
 export default Persons
