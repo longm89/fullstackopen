1a0edd981d1adb8b5f43e11d78eec1d39c6560a7
Long Mai <longm89@gmail.com>
2021-02-17 21:18:05 +0100

    finish exercise 4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+require("dotenv").config()
+const Blog = require("./model/blog")
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    console.log(request.body)
+    const blog = new Blog(request.body)
+    console.log(blog)
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
