c9c808d52f4ced7df90ff1293a8848487e7d102e
Long Mai <longm89@gmail.com>
2021-02-24 12:27:11 +0100

    add ex 4.15, 4.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
@@ -17,4 +17,5 @@ mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users',usersRouter)
 module.exports = app
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
